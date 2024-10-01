4a12bab27c2be81a6752db07cec65b6dad085111
simmonsrour <128705106+simmonsrour@users.noreply.github.com>
2024-10-01 15:53:56 -0300

    Feat: app.py

--- actividades/ej_01/app.py
+++ actividades/ej_01/app.py
@@ -1,1 +1,12 @@
 # Aplicacion del servidor
+from microdot import Microdot, send_file
+from machine import Pin
+
+app = Microdot()
+
+@app.route('/')
+async def index(request):
+    return 'Hello, world!'
+
+
+app.run(port = 80)# Aplicacion del servidor# Aplicacion del servidor
